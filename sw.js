ffa456a1cd58feba54095be7b434cd74cf59180e
Enzox2006 <vdimatteo22@gmail.com>
2026-05-06 10:30:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,34 @@
 const CACHE_NAME = "avanti-altro-v17";
+// Usa percorsi relativi (senza lo slash iniziale) per evitare 404 in sottocartelle
 const ASSETS = [
-    "/",
-    "/index.html",
-    "/manifest.json",
-    "/src/style.css",
-    "/src/domande.js",
-    "/src/field.js",
-    "/src/players.js",
-    "/src/grafica.js",
-    "/src/main.js",
-    "/src/key.js",
-    "/img/quizshow.png",
-    "/img/sfera.png",
-    "/img/background.png",
-    "/img/back.png",
-    "/img/cuboblu.png",
-    "/img/cubogia.png",
-    "/img/cubogrn.png",
-    "/img/cuborux.png"
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./src/style.css",
+    "./src/domande.js",
+    "./src/field.js",
+    "./src/players.js",
+    "./src/grafica.js",
+    "./src/main.js",
+    "./src/key.js",
+    "./img/quizshow.png",
+    "./img/sfera.png",
+    "./img/background.png",
+    "./img/back.png",
+    "./img/cuboblu.png",
+    "./img/cubogia.png",
+    "./img/cubogrn.png",
+    "./img/cuborux.png"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                // addAll fallisce se anche un solo file restituisce 404
+                return cache.addAll(ASSETS);
+            })
+            .catch(err => console.error("Errore cache.addAll: assicurati che tutti i percorsi siano corretti!", err))
     );
     self.skipWaiting();
 });
@@ -38,15 +44,20 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     if (event.request.method !== "GET") return;
+    
     event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                if (response && response.status === 200) {
-                    const clone = response.clone();
-                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        caches.match(event.request).then(cachedResponse => {
+            // Strategia: Cache First, poi Network (ottimale per gioco offline)
+            if (cachedResponse) return cachedResponse;
+
+            return fetch(event.request).then(response => {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
                 }
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
                 return response;
-            })
-            .catch(() => caches.match(event.request))
+            });
+        })
     );
 });
