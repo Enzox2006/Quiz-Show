4ad2f31d233e95a3f0cd6231b44e6df5c0142427
Enzox2006 <vdimatteo22@gmail.com>
2026-05-09 18:34:46 +0200

    Delete src directory

--- src/domande_ruota.js
+++
@@ -1,9 +0,0 @@
-const FRASI_RUOTA = [
-    { frase: "VENEZIA", categoria: "Città Italiane" },
-    { frase: "FIRENZE", categoria: "Città Italiane" },
-    { frase: "NAPOLI", categoria: "Città Italiane" },
-    { frase: "PALERMO", categoria: "Città Italiane" },
-    { frase: "TORINO", categoria: "Città Italiane" },
-    { frase: "BOLOGNA", categoria: "Città Italiane" },
-    { frase: "BARI", categoria: "Città Italiane" },
-];
