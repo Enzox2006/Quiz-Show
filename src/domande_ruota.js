abbafdc3167a44f76514f5129d34dfb2331a95eb
Enzox2006 <vdimatteo22@gmail.com>
2026-05-09 09:15:16 +0200

    Add files via upload

---
+++ src/domande_ruota.js
@@ -0,0 +1,9 @@
+const FRASI_RUOTA = [
+    { frase: "VENEZIA", categoria: "Città Italiane" },
+    { frase: "FIRENZE", categoria: "Città Italiane" },
+    { frase: "NAPOLI", categoria: "Città Italiane" },
+    { frase: "PALERMO", categoria: "Città Italiane" },
+    { frase: "TORINO", categoria: "Città Italiane" },
+    { frase: "BOLOGNA", categoria: "Città Italiane" },
+    { frase: "BARI", categoria: "Città Italiane" },
+];
